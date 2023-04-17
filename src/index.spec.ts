fd2fc478924163c2b368508f2e47aa2c452e48be
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-04-17 22:38:19 +0200

    Minor formatting

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
-import makeEventProps from '.';
+import makeEventProps from './index';
 
 describe('makeEventProps', () => {
   const fakeEvent = {};
