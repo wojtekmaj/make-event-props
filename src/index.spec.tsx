57c355b57df1e7a1efe5ee141ed7d2b738f3617e
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-29 16:16:17 +0200

    Use native ESM in unit tests

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest';
 import React from 'react';
-import makeEventProps, { allEvents } from './index';
+import makeEventProps, { allEvents } from './index.js';
 
 describe('makeEventProps()', () => {
   const fakeEvent = {};
